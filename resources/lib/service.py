08f797196cacc06118b669ed539b7f04866f93c1
meskman <mesk.trash@gmail.com>
2019-11-28 17:30:50 +0200

    Commitigne that refactoring

--- resources/lib/service.py
+++ resources/lib/service.py
@@ -10,7 +10,6 @@ import urllib2
 import re
 import os
 
-
 ADDON = xbmcaddon.Addon()
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 
@@ -21,7 +20,8 @@ def loop():
     while not monitor.abortRequested():
         # Sleep/wait for abort for 10 seconds
         if kodiutils.get_setting_as_bool("debug"):
-            kodiutils.notification("Edem.tv parser", "Debug :: Timer set to %d" % kodiutils.get_setting_as_int("timer"), time=5000, icon=ADDON.getAddonInfo('icon'),
+            kodiutils.notification("Edem.tv parser", "Debug :: Timer set to %d" % kodiutils.get_setting_as_int("timer"),
+                                   time=5000, icon=ADDON.getAddonInfo('icon'),
                                    sound=True)
         if monitor.waitForAbort(kodiutils.get_setting_as_int("timer")):
             # Abort was requested while waiting. We should exit
@@ -31,41 +31,53 @@ def loop():
         parse()
 
 
+def get_m3u_playlist(m3uUrl):
+    # Getting playlist from URL
+    req = urllib2.Request(m3uUrl)
+    req.add_header('User-Agent',
+                   ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    data = response.read()
+    return data
+
+
+def get_wishlist():
+    # Getting wishlist
+    path = os.path.join(kodiutils.get_setting("wlPath"), kodiutils.get_setting("wlFilename"))
+    if kodiutils.get_setting("wlPath") == "":
+        kodiutils.notification("Warning", "Please set a path where to store a wishlist", time=5000,
+                               icon=ADDON.getAddonInfo('icon'), sound=True)
+        path = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')), kodiutils.get_setting("wlFilename"))
+    logger.debug(path)
+    if os.path.exists(path):
+        mode = 'r'
+    else:
+        mode = 'w+'
+    with open(path, mode) as f:
+        f_ch = f.read()
+        return f_ch
+
+
 def parse():
     logger.info("Edem.tv.parser is executed at %s" % time.time())
+    # Showing debug notifications. Controlled by settings.
     if kodiutils.get_setting_as_bool("debug"):
         kodiutils.notification("Edem.tv parser", "Debug :: Check", time=5000, icon=ADDON.getAddonInfo('icon'),
-                           sound=True)
+                               sound=True)
     m3uUrl = kodiutils.get_setting("m3uUrl")
+    # URL should be set in settings. Do nothing if empty and show notification
     if m3uUrl == "":
         kodiutils.notification("Warning", "Please set a Edem.tv M3U Play List URL", time=5000,
                                icon=ADDON.getAddonInfo('icon'), sound=True)
     else:
-        # logger.debug("[Edem.tv.parser] %s" % m3uUrl)
+        data = get_m3u_playlist(m3uUrl)
         channels = {}
-        #path = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')), "my_channels.txt")
-        path = os.path.join(kodiutils.get_setting("wlPath"), kodiutils.get_setting("wlFilename"))
-        if kodiutils.get_setting("wlPath") == "":
-            kodiutils.notification("Warning", "Please set a path where to store a wishlist", time=5000,
-                                   icon=ADDON.getAddonInfo('icon'), sound=True)
-            path = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')), kodiutils.get_setting("wlFilename"))
-        logger.debug(path)
-        if os.path.exists(path):
-            mode = 'r'
-        else:
-            mode = 'w+'
-        with open(path, mode) as f:
-            f_ch = f.read()
+        f_ch = get_wishlist()
         # logger.debug(f_ch)
         match = re.finditer("(.*)\:(.*)", f_ch)
         for item in match:
             channels[item.group(1)] = item.group(2)
-        # m3uUrl = "http://66339a5771d1.yourlistbest.net/playlists/uplist/a4794bfc68406228118598a1bf8c1737/playlist.m3u8"
-        req = urllib2.Request(m3uUrl)
-        req.add_header('User-Agent',
-                       ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        data = response.read()
+
         # logger.debug(data)
         plylist_path = os.path.join(kodiutils.get_setting("m3uPath"), kodiutils.get_setting("m3uFilename"))
         if kodiutils.get_setting("m3uPath") == "":
@@ -85,7 +97,7 @@ def parse():
             # print channel_name
             for key in channels:
                 if channel_name[10:] == key:
-                    logger.debug("%s is found in %s" % (channel_name.decode('utf-8'), path))
+                    logger.debug("%s is found" % (channel_name.decode('utf-8')))
                     if channel_group[8:] != channels[key]:
                         logger.debug("Group is not the same for %s" % channel_name.decode('utf-8'))
                         channel_group = "#EXTGRP:%s" % channels[key]
@@ -94,6 +106,7 @@ def parse():
                         logger.debug("Group is the same %s" % channel_name.decode('utf-8'))
 
                 else:
-                    logger.debug("%s is NOT found in %s" % (channel_name.decode('utf-8'), path))
-            f.write('#EXTINF:0 group-title="' + channel_group[8:] + '",' + channel_name[10:] +"\n"+ channel_group +"\n"+ channel_link +"\n")
+                    logger.debug("%s is NOT found" % (channel_name.decode('utf-8')))
+            f.write('#EXTINF:0 group-title="' + channel_group[8:] + '",' + channel_name[
+                                                                           10:] + "\n" + channel_group + "\n" + channel_link + "\n")
         f.close()
