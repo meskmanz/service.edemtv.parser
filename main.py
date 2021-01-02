7f064d5f2cd55d5a863b73c952f8e32a98c49f79
Meskman <mesk.trash@gmail.com>
2021-01-02 20:28:32 +0200

    v1.0.5

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import xbmcaddon
 # doesn't keep a compiled copy of this
 ADDON = xbmcaddon.Addon()
 kodilogging.config()
-kodiutils.notification("Edem.tv parser", "Launching the app", time=5000, icon=ADDON.getAddonInfo('icon'), sound=True)
+kodiutils.notification(kodiutils.get_string(30032), kodiutils.get_string(30031), time=5000, icon=ADDON.getAddonInfo('icon'), sound=True)
 service.parse()
 service.loop()
 
--- resources/lib/service.py
+++ resources/lib/service.py
@@ -20,7 +20,7 @@ def loop():
     while not monitor.abortRequested():
         # Sleep/wait for abort for 10 seconds
         if kodiutils.get_setting_as_bool("debug"):
-            kodiutils.notification("Edem.tv parser", "Debug :: Timer set to %d" % kodiutils.get_setting_as_int("timer"),
+            kodiutils.notification(kodiutils.get_string(30032), "Debug :: Timer set to %d" % kodiutils.get_setting_as_int("timer"),
                                    time=5000, icon=ADDON.getAddonInfo('icon'),
                                    sound=True)
         if monitor.waitForAbort(kodiutils.get_setting_as_int("timer")):
@@ -62,12 +62,12 @@ def parse():
     logger.info("Edem.tv.parser is executed at %s" % time.time())
     # Showing debug notifications. Controlled by settings.
     if kodiutils.get_setting_as_bool("debug"):
-        kodiutils.notification("Edem.tv parser", "Debug :: Check", time=5000, icon=ADDON.getAddonInfo('icon'),
+        kodiutils.notification(kodiutils.get_string(30032), "Debug :: Check", time=5000, icon=ADDON.getAddonInfo('icon'),
                                sound=True)
     m3uUrl = kodiutils.get_setting("m3uUrl")
     # URL should be set in settings. Do nothing if empty and show notification
     if m3uUrl == "":
-        kodiutils.notification("Warning", "Please set a Edem.tv M3U Play List URL", time=5000,
+        kodiutils.notification(kodiutils.get_string(30033), kodiutils.get_string(30034), time=5000,
                                icon=ADDON.getAddonInfo('icon'), sound=True)
     else:
         data = get_m3u_playlist(m3uUrl)
@@ -80,7 +80,7 @@ def parse():
         # logger.debug(data)
         plylist_path = os.path.join(kodiutils.get_setting("m3uPath"), kodiutils.get_setting("m3uFilename"))
         if kodiutils.get_setting("m3uPath") == "":
-            kodiutils.notification("Warning", "Please set a path where to store a playlist", time=5000,
+            kodiutils.notification(kodiutils.get_string(30033), kodiutils.get_string(30035), time=5000,
                                    icon=ADDON.getAddonInfo('icon'), sound=True)
             plylist_path = os.path.join(xbmc.translatePath(ADDON.getAddonInfo('profile')),
                                         kodiutils.get_setting("m3uFilename"))
@@ -92,7 +92,14 @@ def parse():
         f.write("#EXTM3U\n")
         pattern = "(\#EXTINF\:0.*\,.*)\n(\#EXTGRP\:.*)\n(http\:\/\/.*)\n"
         match = re.finditer(pattern, data, re.M | re.I)
+
+        stat_items_before = 0
+        stat_items_after = 0
+        stat_items_excluded1 = 0
+        stat_items_excluded2 = 0
+
         for item in match:
+            stat_items_before += 1
             channel_name = item.group(1).replace("\r", "")
             channel_group = item.group(2).replace("\r", "")
             channel_link = item.group(3).replace("\r", "")
@@ -102,6 +109,7 @@ def parse():
             ch_name = channel_name.split(",")[1]
             exclude_channel = bool(re.match(exclude_ch_pattern, ch_name))
             if len(exclude_ch_pattern) > 0 and exclude_channel:
+                stat_items_excluded1 += 1
                 write = False
             if kodiutils.get_setting_as_bool("debug"):
                 logger.debug(ch_name + ":" + channel_group[8:] + '\n')
@@ -111,6 +119,7 @@ def parse():
                     logger.debug("%s is found" % ch_name)
                     if channel_group[8:] != channels[key]:
                         if channels[key] == "delete".lower():
+                            stat_items_excluded2 += 1
                             write = False
                         logger.debug("Group is not the same for %s" % ch_name)
                         channel_group = "#EXTGRP:%s" % channels[key]
@@ -120,6 +129,10 @@ def parse():
                 else:
                     logger.debug("%s is NOT found" % ch_name)
             if write:
+                stat_items_after += 1
                 f.write(ch_code + ' group-title="' + channel_group[
                                                      8:] + '",' + ch_name + '\n' + channel_group + "\n" + channel_link + "\n")
         f.close()
+        ch_results = "Channels on start:" + str(stat_items_before)+ "\nChannels ready:" + str(stat_items_after) +\
+                     "\nChannels excluded:" + str(stat_items_excluded1)+ "\nChannels deleted:" + str(stat_items_excluded2)
+        logger.info(ch_results)
